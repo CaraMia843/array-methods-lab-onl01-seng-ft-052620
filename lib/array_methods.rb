88d568164fee7b717c0af73e923ee5688a0a509c
Cara Zellner <carajeanine@gmail.com>
2020-06-01 15:37:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,3 +1,8 @@
+def using_include(array. element)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.include?("wow")
+end
+
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.sort
